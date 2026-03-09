66ce59bab2af9d6896d3c7f9e12c1449657475a3
JenniferRoe <jenniferroesner85@yahoo.com>
2026-03-09 10:58:27 +0100

    4d/Delete_options

--- 04_CRUD/04d_CRUD_delete.sql
+++ 04_CRUD/04d_CRUD_delete.sql
@@ -1,4 +1,8 @@
+\! cls
 /* D - DELETE */
+
+
+-- SELECT * FROM boo.customers LIMIT 5;
 
 # Einzelne Zeilen
 -- DELETE FROM boo.customers WHERE id = 3; -- Filter
@@ -7,3 +11,12 @@
 # Alle Zeilen
 -- DELETE FROM boo.customers; -- ACHTUNG Daten sind weg!!
 -- SELECT * FROM boo.customers;
+-- DESCRIBE boo.customers; -- Tabelle ist noch da, aber leer
+
+-- Optimiertes Löschen aller Zeilen
+-- TRUNCATE TABLE boo.customers; -- Alle Zeilen löschen, aber Tabelle bleibt erhalten
+-- SELECT * FROM boo.customers; -- Tabelle ist noch da, aber leer  
+
+
+-- DROP TABLE boo.customers; -- Tabelle  und Daten sind weg
+-- DELETE FROM boo.customers; -- Alle Zeilen löschen, aber Tabelle bleibt erhalten  
